1747c8961e00ddfc8802e0ade56946ec040036d8
Hector Margittay <hector.margittay@estagiario.cidadania.gov.br>
2019-11-12 09:33:33 -0300

    Mockup da Sidebar e Navbar

---
+++ mockup/js/main.js
@@ -0,0 +1,14 @@
+$(()=>{
+    let close_sidenav_callback = function(){
+        $('#sidenav').removeClass('active');
+        $('#sidenav-overlay').removeClass('active');
+    };
+
+    $('#sidenav-overlay, #sidenav-dismiss').on('click', close_sidenav_callback);
+    $(window).resize(close_sidenav_callback);
+
+    $('#sidenav-toggle').on('click', function(){
+        $('#sidenav').addClass('active');
+        $('#sidenav-overlay').addClass('active');
+    });
+});
